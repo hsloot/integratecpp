62ead3b4acb9cb6dbd08909f4dcc7aa7914bc4c5
hsloot <henrik.sloot@gmail.com>
2022-06-19 00:52:50 +0200

    use testutils package approach

--- tests/testthat/testutils.integratecpp/R/package.R
+++ tests/testthat/testutils.integratecpp/R/package.R
@@ -15,23 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#' Throw exception and catch error message
-#'
-#' @param exception name of the exception to be thrown.
-#' @param message error message passed to the exception constructor.
-#'
-#' @keywords internal
-catch_what <- function(exception, message) {
-    stopifnot(exception %in% c(
-        "integration_logic_error",
-        "integration_runtime_error",
-        "max_subdivision_error",
-        "roundoff_error",
-        "bad_integrand_error",
-        "extrapolation_roundoff_error",
-        "divergence_error",
-        "invalid_input_error"
-    ))
-
-    get(paste("Rcpp", exception, "catch_what", sep = "__"))(message)
-}
+## usethis namespace: start
+#' @useDynLib testutils.integratecpp, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
