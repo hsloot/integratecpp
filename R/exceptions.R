a65b9a9a397b1276eaa5191f55dcb805bc063f1c
hsloot <henrik.sloot@gmail.com>
2022-06-19 14:21:48 +0200

    use GPL (>= 3)

--- R/exceptions.R
+++ R/exceptions.R
@@ -1,5 +1,19 @@
-## REVIEW: consider moving file to `inst/Rsource`
-##   and load specifically as test-helper.
+# Copyright (C) 2022 Henrik Sloot
+#
+# This file is part of integratecpp
+#
+# integratecpp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# integratecpp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #' Throw exception and catch error message
 #'
--- inst/include/integratecpp.h
+++ inst/include/integratecpp.h
@@ -1,10 +1,27 @@
+// Copyright (C) 2022 Henrik Sloot
+//
+// This file is part of integratecpp
+//
+// integratecpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// integratecpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 /*!
  * \file integratecpp.h
  *
  * \author      Henrik Sloot
  * \date        2022
  * \copyright   Copyright 2022 Henrik Sloot. All rights reserved.
- *              This file is licensed under the terms of the MIT license.
+ *              This project is released under the GNU Public License.
  *
  * \note        *API* consists of all public classes, types and methods in the
  *              namespace `integratecpp` which are not contained in
