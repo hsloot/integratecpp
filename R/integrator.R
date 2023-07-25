e3516b87b42a0251341f511080b1dd077385ebb0
hsloot <16246049+hsloot@users.noreply.github.com>
2023-07-25 11:58:55 +0200

    style: resolve lints

--- R/integrator.R
+++ R/integrator.R
@@ -29,18 +29,18 @@ Integrator <- setClass("Integrator", slots = c("pointer" = "externalptr")) # nol
 #' @keywords internal
 #' @noRd
 setValidity("Integrator", function(object) {
-    if (identical(object@pointer, new("externalptr"))) {
-        return("dangling pointer")
-    } else {
-        return(tryCatch(
-            Rcpp__integrator__throw_if_invalid(object@pointer),
-            error = function(cond) {
-                return(as.character(cond))
-            }
-        ))
-    }
+  if (identical(object@pointer, new("externalptr"))) {
+    return("dangling pointer")
+  } else {
+    return(tryCatch(
+      Rcpp__integrator__throw_if_invalid(object@pointer),
+      error = function(cond) {
+        return(as.character(cond))
+      }
+    ))
+  }
 
-    invisible(TRUE)
+  invisible(TRUE)
 })
 
 #' @describeIn Integrator-class
@@ -50,10 +50,10 @@ setValidity("Integrator", function(object) {
 #' @importFrom methods setMethod validObject
 #' @keywords internal
 setMethod("initialize", "Integrator", function(.Object, max_subdivisions = 100, relative_accuracy = .Machine$double.eps^0.25, absolute_accuracy = relative_accuracy, work_size = 4 * max_subdivisions) { # nolint
-    .Object@pointer <- Rcpp__integrator__new(max_subdivisions, relative_accuracy, absolute_accuracy, work_size) # nolint
-    validObject(.Object)
+  .Object@pointer <- Rcpp__integrator__new(max_subdivisions, relative_accuracy, absolute_accuracy, work_size) # nolint
+  validObject(.Object)
 
-    .Object
+  .Object
 })
 
 #' @describeIn Integrator-class
@@ -67,26 +67,26 @@ setMethod("initialize", "Integrator", function(.Object, max_subdivisions = 100, 
 #'
 #' @keywords internal
 setMethod("$", "Integrator", function(x, name) {
-    if (name %in% c("max_subdivisions", "relative_accuracy", "absolute_accuracy", "work_size")) { # nolint
-        get(paste("Rcpp__integrator__get", name, sep = "_"))(x@pointer)
-    } else if (name == "integrate") {
-        function(f, lower, upper, ..., stop_on_error = TRUE) { # nolint
-            out <- Rcpp__integrator__integrate(
-                x@pointer,
-                function(y) f(y, ...), lower, upper
-            )
-            out$call <- match.call()
-            class(out) <- "integrate"
+  if (name %in% c("max_subdivisions", "relative_accuracy", "absolute_accuracy", "work_size")) { # nolint
+    get(paste("Rcpp__integrator__get", name, sep = "_"))(x@pointer)
+  } else if (name == "integrate") {
+    function(f, lower, upper, ..., stop_on_error = TRUE) { # nolint
+      out <- Rcpp__integrator__integrate(
+        x@pointer,
+        function(y) f(y, ...), lower, upper
+      )
+      out$call <- match.call()
+      class(out) <- "integrate"
 
-            if (isTRUE(stop_on_error) && !isTRUE(out$message == "OK")) {
-                stop(out$message)
-            }
+      if (isTRUE(stop_on_error) && !isTRUE(out$message == "OK")) {
+        stop(out$message)
+      }
 
-            out
-        }
-    } else {
-        stop("not implemented") # nocov
+      out
     }
+  } else {
+    stop("not implemented") # nocov
+  }
 })
 
 #' @describeIn Integrator-class
@@ -99,14 +99,14 @@ setMethod("$", "Integrator", function(x, name) {
 #'
 #' @keywords internal
 setMethod("$<-", "Integrator", function(x, name, value) {
-    if (name %in% c("max_subdivisions", "relative_accuracy", "absolute_accuracy", "work_size")) { # nolint
-        get(paste("Rcpp__integrator__set", name, sep = "_"))(x@pointer, value)
-        validObject(x)
+  if (name %in% c("max_subdivisions", "relative_accuracy", "absolute_accuracy", "work_size")) { # nolint
+    get(paste("Rcpp__integrator__set", name, sep = "_"))(x@pointer, value)
+    validObject(x)
 
-        x
-    } else {
-        stop("not implemented") # nocov
-    }
+    x
+  } else {
+    stop("not implemented") # nocov
+  }
 })
 
 # nocov start
@@ -121,18 +121,18 @@ setMethod("$<-", "Integrator", function(x, name, value) {
 #' @keywords internal
 #' @noRd
 setMethod("show", "Integrator", function(object) {
-    cat(sprintf("An object of class %s\n", classLabel(class(object))))
-    if (isTRUE(validObject(object, test = TRUE))) {
-        cat(sprintf("%s\n", format(object@pointer)))
-        cat(sprintf("- max_subdivisions: %s\n", format(object$max_subdivisions)))
-        cat(sprintf("- relative_accuracy: %s\n", format(object$relative_accuracy, scientific = TRUE)))
-        cat(sprintf("- absolute_accuracy: %s\n", format(object$absolute_accuracy, scientific = TRUE)))
-        cat(sprintf("- work_size: %s\n", format(object$work_size)))
-    } else {
-        cat("\t (invalid or not initialized)\n")
-    }
+  cat(sprintf("An object of class %s\n", classLabel(class(object))))
+  if (isTRUE(validObject(object, test = TRUE))) {
+    cat(sprintf("%s\n", format(object@pointer)))
+    cat(sprintf("- max_subdivisions: %s\n", format(object$max_subdivisions)))
+    cat(sprintf("- relative_accuracy: %s\n", format(object$relative_accuracy, scientific = TRUE)))
+    cat(sprintf("- absolute_accuracy: %s\n", format(object$absolute_accuracy, scientific = TRUE)))
+    cat(sprintf("- work_size: %s\n", format(object$work_size)))
+  } else {
+    cat("\t (invalid or not initialized)\n")
+  }
 
-    invisible(NULL)
+  invisible(NULL)
 })
 
 # nolint end
--- tests/testthat/test-integrate.R
+++ tests/testthat/test-integrate.R
@@ -16,273 +16,273 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 remove_call <- function(x) {
-    x <- x[-match(x, "call")]
+  x <- x[-match(x, "call")]
 }
 
 test_that("Default settings for exponential distribtion's expectation", {
-    fn <- function(x, rate = 1) {
-        x * dexp(x, rate = rate)
-    }
+  fn <- function(x, rate = 1) {
+    x * dexp(x, rate = rate)
+  }
 
-    expect_equal(
-        remove_call(integrate(fn, 0, Inf, rate = 0.5)),
-        remove_call(stats::integrate(fn, 0, Inf, rate = 0.5))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, Inf, rate = 0.5)),
+    remove_call(stats::integrate(fn, 0, Inf, rate = 0.5))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, 0, Inf, rate = 1)),
-        remove_call(stats::integrate(fn, 0, Inf, rate = 1))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, Inf, rate = 1)),
+    remove_call(stats::integrate(fn, 0, Inf, rate = 1))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, 0, Inf, rate = 2)),
-        remove_call(stats::integrate(fn, 0, Inf, rate = 2))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, Inf, rate = 2)),
+    remove_call(stats::integrate(fn, 0, Inf, rate = 2))
+  )
 })
 
 test_that("Default settings for beta distribtion's expectation", {
-    fn <- function(x, shape1, shape2) {
-        x * dbeta(x, shape1 = shape1, shape2 = shape2)
-    }
+  fn <- function(x, shape1, shape2) {
+    x * dbeta(x, shape1 = shape1, shape2 = shape2)
+  }
 
-    expect_equal(
-        remove_call(integrate(fn, 0, 1, shape1 = 1, shape2 = 1)),
-        remove_call(stats::integrate(fn, 0, 1, shape1 = 1, shape2 = 1))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, 1, shape1 = 1, shape2 = 1)),
+    remove_call(stats::integrate(fn, 0, 1, shape1 = 1, shape2 = 1))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, 0, 1, shape1 = 0.3, shape2 = 0.4)),
-        remove_call(stats::integrate(fn, 0, 1, shape1 = 0.3, shape2 = 0.4))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, 1, shape1 = 0.3, shape2 = 0.4)),
+    remove_call(stats::integrate(fn, 0, 1, shape1 = 0.3, shape2 = 0.4))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, 0, 1, shape1 = 1.5, shape2 = 2)),
-        remove_call(stats::integrate(fn, 0, 1, shape1 = 1.5, shape2 = 2))
-    )
+  expect_equal(
+    remove_call(integrate(fn, 0, 1, shape1 = 1.5, shape2 = 2)),
+    remove_call(stats::integrate(fn, 0, 1, shape1 = 1.5, shape2 = 2))
+  )
 })
 
 test_that("Default settings for negative Weibull distribtion's expectation", {
-    fn <- function(x, shape, scale = 1) {
-        x * dweibull(-x, shape = shape, scale = scale)
-    }
+  fn <- function(x, shape, scale = 1) {
+    x * dweibull(-x, shape = shape, scale = scale)
+  }
 
-    expect_equal(
-        remove_call(integrate(fn, -Inf, 0, shape = 1, scale = 1)),
-        remove_call(stats::integrate(fn, -Inf, 0, shape = 1, scale = 1))
-    )
+  expect_equal(
+    remove_call(integrate(fn, -Inf, 0, shape = 1, scale = 1)),
+    remove_call(stats::integrate(fn, -Inf, 0, shape = 1, scale = 1))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, -Inf, 0, shape = 0.3, scale = 0.4)),
-        remove_call(stats::integrate(fn, -Inf, 0, shape = 0.3, scale = 0.4))
-    )
+  expect_equal(
+    remove_call(integrate(fn, -Inf, 0, shape = 0.3, scale = 0.4)),
+    remove_call(stats::integrate(fn, -Inf, 0, shape = 0.3, scale = 0.4))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, -Inf, 0, shape = 1.5, scale = 2)),
-        remove_call(stats::integrate(fn, -Inf, 0, shape = 1.5, scale = 2))
-    )
+  expect_equal(
+    remove_call(integrate(fn, -Inf, 0, shape = 1.5, scale = 2)),
+    remove_call(stats::integrate(fn, -Inf, 0, shape = 1.5, scale = 2))
+  )
 })
 
 test_that("Less max. subdivisions for gamma distribution's expectation", {
-    fn <- function(x, shape, rate = 1) {
-        x * dgamma(x, shape = shape, rate = rate)
-    }
+  fn <- function(x, shape, rate = 1) {
+    x * dgamma(x, shape = shape, rate = rate)
+  }
 
-    expect_equal(
-        remove_call(integrate(
-            fn, 0, Inf,
-            shape = 0.3, rate = 0.5,
-            max_subdivisions = 50L
-        )),
-        remove_call(stats::integrate(
-            fn, 0, Inf,
-            shape = 0.3, rate = 0.5,
-            subdivisions = 50L
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, 0, Inf,
+      shape = 0.3, rate = 0.5,
+      max_subdivisions = 50L
+    )),
+    remove_call(stats::integrate(
+      fn, 0, Inf,
+      shape = 0.3, rate = 0.5,
+      subdivisions = 50L
+    ))
+  )
 
-    expect_equal(
-        remove_call(integrate(
-            fn, 0, Inf,
-            shape = 0.7, rate = 1.5,
-            max_subdivisions = 50L
-        )),
-        remove_call(stats::integrate(
-            fn, 0, Inf,
-            shape = 0.7, rate = 1.5,
-            subdivisions = 50L
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, 0, Inf,
+      shape = 0.7, rate = 1.5,
+      max_subdivisions = 50L
+    )),
+    remove_call(stats::integrate(
+      fn, 0, Inf,
+      shape = 0.7, rate = 1.5,
+      subdivisions = 50L
+    ))
+  )
 })
 
 test_that("More max. subdivisions for chi-square distribution's expectation", {
-    fn <- function(x, df, ncp = 0) {
-        x * dchisq(x, df = df, ncp = ncp)
-    }
+  fn <- function(x, df, ncp = 0) {
+    x * dchisq(x, df = df, ncp = ncp)
+  }
 
-    expect_equal(
-        remove_call(integrate(fn, 0, Inf, df = 3, max_subdivisions = 200L)),
-        remove_call(
-            stats::integrate(fn, 0, Inf, df = 3, subdivisions = 200L)
-        )
+  expect_equal(
+    remove_call(integrate(fn, 0, Inf, df = 3, max_subdivisions = 200L)),
+    remove_call(
+      stats::integrate(fn, 0, Inf, df = 3, subdivisions = 200L)
     )
+  )
 
-    expect_equal(
-        remove_call(
-            integrate(fn, 0, Inf, df = 5, ncp = 2, max_subdivisions = 200L)
-        ),
-        remove_call(
-            stats::integrate(fn, 0, Inf, df = 5, ncp = 2, subdivisions = 200L)
-        )
+  expect_equal(
+    remove_call(
+      integrate(fn, 0, Inf, df = 5, ncp = 2, max_subdivisions = 200L)
+    ),
+    remove_call(
+      stats::integrate(fn, 0, Inf, df = 5, ncp = 2, subdivisions = 200L)
     )
+  )
 })
 
 test_that("Smaller required rel. tol. for normal distribution's variance", {
-    fn <- function(x, mean = 0, sd = 1) {
-        (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
-    }
+  fn <- function(x, mean = 0, sd = 1) {
+    (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
+  }
 
-    expect_equal(
-        remove_call(integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 2,
-            relative_accuracy = .Machine$double.eps^0.125
-        )),
-        remove_call(stats::integrate(
-            fn, 0, Inf,
-            mean = 0, sd = 2,
-            rel.tol = .Machine$double.eps^0.125
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 2,
+      relative_accuracy = .Machine$double.eps^0.125
+    )),
+    remove_call(stats::integrate(
+      fn, 0, Inf,
+      mean = 0, sd = 2,
+      rel.tol = .Machine$double.eps^0.125
+    ))
+  )
 
-    expect_equal(
-        remove_call(integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 0.5,
-            relative_accuracy = .Machine$double.eps^0.125
-        )),
-        remove_call(stats::integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 0.5,
-            rel.tol = .Machine$double.eps^0.125
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 0.5,
+      relative_accuracy = .Machine$double.eps^0.125
+    )),
+    remove_call(stats::integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 0.5,
+      rel.tol = .Machine$double.eps^0.125
+    ))
+  )
 })
 
 test_that("Higher required rel. tol. for normal distribution's variance", {
-    fn <- function(x, mean = 0, sd = 1) {
-        (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
-    }
+  fn <- function(x, mean = 0, sd = 1) {
+    (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
+  }
 
-    expect_equal(
-        remove_call(integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 2,
-            relative_accuracy = .Machine$double.eps^0.5
-        )),
-        remove_call(stats::integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 2,
-            rel.tol = .Machine$double.eps^0.5
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 2,
+      relative_accuracy = .Machine$double.eps^0.5
+    )),
+    remove_call(stats::integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 2,
+      rel.tol = .Machine$double.eps^0.5
+    ))
+  )
 
-    expect_equal(
-        remove_call(integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 0.5,
-            relative_accuracy = .Machine$double.eps^0.5
-        )),
-        remove_call(stats::integrate(
-            fn, -Inf, Inf,
-            mean = 0, sd = 0.5,
-            rel.tol = .Machine$double.eps^0.5
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 0.5,
+      relative_accuracy = .Machine$double.eps^0.5
+    )),
+    remove_call(stats::integrate(
+      fn, -Inf, Inf,
+      mean = 0, sd = 0.5,
+      rel.tol = .Machine$double.eps^0.5
+    ))
+  )
 })
 
 test_that("Set required abs. tol. to zero for normal distribution's variance", {
-    fn <- function(x, mean = 0, sd = 1) {
-        (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
-    }
+  fn <- function(x, mean = 0, sd = 1) {
+    (x - mean)^2 * dnorm(x, mean = mean, sd = sd)
+  }
 
-    expect_equal(
-        remove_call(integrate(fn, -Inf, Inf,
-            mean = 0, sd = 2,
-            absolute_accuracy = 0
-        )),
-        remove_call(stats::integrate(fn, 0, Inf,
-            mean = 0, sd = 2,
-            abs.tol = 0
-        ))
-    )
+  expect_equal(
+    remove_call(integrate(fn, -Inf, Inf,
+      mean = 0, sd = 2,
+      absolute_accuracy = 0
+    )),
+    remove_call(stats::integrate(fn, 0, Inf,
+      mean = 0, sd = 2,
+      abs.tol = 0
+    ))
+  )
 
-    expect_equal(
-        remove_call(integrate(fn, -Inf, Inf,
-            mean = 0, sd = 0.5,
-            absolute_accuracy = 0
-        )),
-        remove_call(
-            stats::integrate(fn, -Inf, Inf,
-                mean = 0, sd = 0.5,
-                abs.tol = 0
-            )
-        )
+  expect_equal(
+    remove_call(integrate(fn, -Inf, Inf,
+      mean = 0, sd = 0.5,
+      absolute_accuracy = 0
+    )),
+    remove_call(
+      stats::integrate(fn, -Inf, Inf,
+        mean = 0, sd = 0.5,
+        abs.tol = 0
+      )
     )
+  )
 })
 
 test_that("`max_subdivisions == 0` produces `invalid_input_error`", {
-    fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
+  fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
 
-    expect_error(
-        integrate(fn, 0, Inf, rate = 1, max_subdivisions = 0),
-        "the input is invalid"
-    )
+  expect_error(
+    integrate(fn, 0, Inf, rate = 1, max_subdivisions = 0),
+    "the input is invalid"
+  )
 })
 
 test_that("`is.na(lower) || is.na(upper)` produces `invalid_input_error`", {
-    fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
+  fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
 
-    expect_error(
-        integrate(fn, 0, NA, rate = 1),
-        "the input is invalid"
-    )
-    expect_error(
-        integrate(fn, NA, Inf, rate = 1),
-        "the input is invalid"
-    )
+  expect_error(
+    integrate(fn, 0, NA, rate = 1),
+    "the input is invalid"
+  )
+  expect_error(
+    integrate(fn, NA, Inf, rate = 1),
+    "the input is invalid"
+  )
 })
 
 test_that("`eps.abs <= 0 && eps.rel < max(50*.Machine$double.eps, 0.5e-28)` produces `invalid_input_error`", { # nolint
-    fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
+  fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
 
-    expect_error(
-        integrate(
-            fn, 0, Inf,
-            rate = 1,
-            absolute_accuracy = 0,
-            relative_accuracy = 0.5 * max(50 * .Machine$double.eps, 0.5e-28)
-        ),
-        "the input is invalid"
-    )
+  expect_error(
+    integrate(
+      fn, 0, Inf,
+      rate = 1,
+      absolute_accuracy = 0,
+      relative_accuracy = 0.5 * max(50 * .Machine$double.eps, 0.5e-28)
+    ),
+    "the input is invalid"
+  )
 })
 
 test_that("`work_size < 4 * max_subdivisions` produces `invalid_input_error`", {
-    fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
+  fn <- function(x, rate = 1) (x - 1 / rate)^2 * dexp(x, rate = rate)
 
-    expect_error(
-        integrate(fn, 0, Inf,
-            rate = 1,
-            max_subdivisions = 100, work_size = 399
-        ),
-        "the input is invalid"
-    )
+  expect_error(
+    integrate(fn, 0, Inf,
+      rate = 1,
+      max_subdivisions = 100, work_size = 399
+    ),
+    "the input is invalid"
+  )
 })
 
 test_that("`max_subdivisions_error` is thrown", {
-    expect_error(
-        integrate(function(x) sin(1 / x), 0, 1),
-        "maximum number of subdivisions reached"
-    )
+  expect_error(
+    integrate(function(x) sin(1 / x), 0, 1),
+    "maximum number of subdivisions reached"
+  )
 })
 
 ## See
@@ -292,61 +292,61 @@ test_that("`max_subdivisions_error` is thrown", {
 ## "[...] terminate early in cases where the exact integral is zero and a
 ## pure relative accuracy is requested (EPSABS=O)."
 test_that("`roundoff_error` is thrown", {
-    expect_error(
-        integrate(function(x) x * dnorm(x), -1, 1, absolute_accuracy = 0),
-        "roundoff error was detected"
-    )
+  expect_error(
+    integrate(function(x) x * dnorm(x), -1, 1, absolute_accuracy = 0),
+    "roundoff error was detected"
+  )
 })
 
 ## See
 ## https://stat.ethz.ch/pipermail/r-help/2013-December/364560.html
 test_that("`bad_integrand_error` is thrown", {
-    expect_error(
-        integrate(function(x) {
-            ## nolint start
-            -((100000 * x)^(-1) - (1000 * x)^(-1)) *
-                1 / (0.20 * sqrt(2 * pi)) *
-                exp(-0.5 * (x - 0.10) / (0.20))^2
-            ## nolint end
-        }, 0, Inf),
-        "extremely bad integrand behaviour"
-    )
+  expect_error(
+    integrate(function(x) {
+      ## nolint start
+      -((100000 * x)^(-1) - (1000 * x)^(-1)) *
+        1 / (0.20 * sqrt(2 * pi)) *
+        exp(-0.5 * (x - 0.10) / (0.20))^2
+      ## nolint end
+    }, 0, Inf),
+    "extremely bad integrand behaviour"
+  )
 })
 
 test_that("`extrapolation_roundoff_error` is thrown", {
-    expect_error(
-        integrate(
-            function(x) {
-                (x - 2000) * dlnorm(x, meanlog = 9.0167, sdlog = 1.6247)
-            },
-            2000, Inf
-        ),
-        "roundoff error is detected in the extrapolation table"
-    )
+  expect_error(
+    integrate(
+      function(x) {
+        (x - 2000) * dlnorm(x, meanlog = 9.0167, sdlog = 1.6247)
+      },
+      2000, Inf
+    ),
+    "roundoff error is detected in the extrapolation table"
+  )
 })
 
 test_that("`divergence_error` is thrown", {
-    expect_error(
-        integrate(function(x) x^-0.9999, 0, 1),
-        "the integral is probably divergent"
-    )
+  expect_error(
+    integrate(function(x) x^-0.9999, 0, 1),
+    "the integral is probably divergent"
+  )
 })
 
 test_that("function evaluation error is thrown", {
-    expect_error(
-        integrate(function(x) stop("stop on purpose"), 0, 1),
-        "Evaluation error: stop on purpose."
-    )
+  expect_error(
+    integrate(function(x) stop("stop on purpose"), 0, 1),
+    "Evaluation error: stop on purpose."
+  )
 })
 
 test_that("non-finite values error is thrown", {
-    expect_error(
-        integrate(
-            function(x) {
-                ifelse(x < 0.3, 1 / (1 - 0.3), ifelse(x > 0.7, 1 / (1 - 0.7), Inf)) # nolint
-            },
-            0, 1
-        ),
-        "non-finite function value"
-    )
+  expect_error(
+    integrate(
+      function(x) {
+        ifelse(x < 0.3, 1 / (1 - 0.3), ifelse(x > 0.7, 1 / (1 - 0.7), Inf)) # nolint
+      },
+      0, 1
+    ),
+    "non-finite function value"
+  )
 })
