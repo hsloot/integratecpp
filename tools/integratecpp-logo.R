9e69347f96794c4d380067b849ec0a7c013d38db
hsloot <16246049+hsloot@users.noreply.github.com>
2023-10-24 00:23:48 +0200

    fix(style): indentation

--- tools/integratecpp-logo.R
+++ tools/integratecpp-logo.R
@@ -37,7 +37,7 @@ library(hexSticker)
 ui_info("Creating logo for package {ui_code('integratecpp')}")
 
 fn <- function(x) {
-  x^-0.7
+    x^-0.7
 }
 
 x <- seq(0, 1, length.out = 100)
@@ -47,33 +47,33 @@ y <- fn(x)
 df <- tibble(x = x, y = y)
 
 p <- df |>
-  ggplot(aes(x = x, y = y)) +
-  geom_area(
-    color = "black",
-    fill = "azure4",
-    alpha = 0.4
-  ) +
-  theme_classic() +
-  theme_transparent() +
-  theme(
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_line(linewidth = 0.25),
-    axis.line = element_line(linewidth = 0.25)
-  )
+    ggplot(aes(x = x, y = y)) +
+    geom_area(
+        color = "black",
+        fill = "azure4",
+        alpha = 0.4
+    ) +
+    theme_classic() +
+    theme_transparent() +
+    theme(
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_line(linewidth = 0.25),
+        axis.line = element_line(linewidth = 0.25)
+    )
 
 logo_path <- here("man", "figures", "logo.png")
 sticker(
-  p,
-  package = "integratecpp",
-  p_size = 20,
-  s_x = 1,
-  s_y = 0.8,
-  s_width = 1.3,
-  s_height = 0.7,
-  h_fill = "darkseagreen3",
-  h_color = "darkseagreen4",
-  filename = logo_path
+    p,
+    package = "integratecpp",
+    p_size = 20,
+    s_x = 1,
+    s_y = 0.8,
+    s_width = 1.3,
+    s_height = 0.7,
+    h_fill = "darkseagreen3",
+    h_color = "darkseagreen4",
+    filename = logo_path
 )
 
 ui_done("Logo saved to {ui_path(logo_path)}")
